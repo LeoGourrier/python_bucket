226d41c1449aa02cd25f388e605da8b0c2de1518
Leo Gourrier <leogourrier@gmail.com>
2022-07-30 18:09:55 +0000

    HR Count Substring - WIP

---
+++ find_a_string.py
@@ -0,0 +1,13 @@
+#Objective: In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. 
+#String traversal will take place from left to right, not from right to left. 
+
+def count_substring(string, sub_string):
+    
+    return
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)

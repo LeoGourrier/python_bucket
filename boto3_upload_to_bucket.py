277554215a9df01f8757267b7ba456a981084a70
Leo Gourrier <leogourrier@gmail.com>
2022-08-01 03:03:06 +0000

    upload file to bucket

---
+++ boto3_upload_to_bucket.py
@@ -0,0 +1,20 @@
+from boto3 import client
+
+#Create a file for uploading
+#test_file = open("test_file.txt", "a")
+#test_file.write("This file is being created to test this upload script")
+#test_file.close()
+
+
+#Upload a single file
+s3 = client("s3")
+s3.upload_file(
+    Filename="test_file.txt",
+    Bucket="thisisanewbucketbucket",
+    Key="uploaded_test_file.txt"
+    )
+
+#List of objects in our created bucket
+objects=s3.list_objects(Bucket="thisisanewbucketbucket")["Contents"]
+for object in objects:
+    print(object['Key'])
